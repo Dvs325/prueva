e32704daaf77aa70f21869afddb0acb14d1fe531
alasd <alasd6411@gmail.com>
2024-07-28 23:43:44 -0600

    provando

---
+++ src/main/java/sv/edu/udb/www/estudiantes/Estudiante.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.www.estudiantes;
+
+public class Estudiante {
+    private String carnet;
+    private String nombre;
+
+    // Getters y setters
+    public String getCarnet() { return carnet; }
+    public void setCarnet(String carnet) { this.carnet = carnet; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre;}
+    }
